fe77b20d799fd529d0d6ac7faf967c446bbe1747
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-09-24 13:15:48 +0100

    Changing URL patterns to include summernote

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
